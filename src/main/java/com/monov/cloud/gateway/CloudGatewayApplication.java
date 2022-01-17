b58b5cd3cd9bb9ec854217cb12a26e07fca7df17
RadoslavMonov <radoslav.monov@estafet.com>
2022-01-17 13:33:50 +0200

    GatewayController remove eureka client annotation

--- src/main/java/com/monov/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/com/monov/cloud/gateway/CloudGatewayApplication.java
@@ -2,12 +2,9 @@ package com.monov.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class CloudGatewayApplication {
 
