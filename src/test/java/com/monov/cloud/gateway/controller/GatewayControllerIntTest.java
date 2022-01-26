9ffbf91aae5a23f4edc0561842da5cfafa180530
RadoslavMonov <radoslav.monov@estafet.com>
2022-01-26 17:08:07 +0200

    Testing add int tests for addStudentToCourse,findStudentsByCourseId

--- src/test/java/com/monov/cloud/gateway/controller/GatewayControllerIntTest.java
+++ src/test/java/com/monov/cloud/gateway/controller/GatewayControllerIntTest.java
@@ -2,8 +2,10 @@ package com.monov.cloud.gateway.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.monov.cloud.gateway.CloudGatewayApplication;
+import com.monov.cloud.gateway.dto.CourseWithStudentDTO;
 import com.monov.commons.dto.CourseDTO;
 import com.monov.commons.dto.CourseSearchRequestDTO;
+import com.monov.commons.dto.ItemIdsDTO;
 import com.monov.commons.dto.StudentDTO;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,17 +28,22 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import static com.monov.commons.utils.StringUtils.asJsonString;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
@@ -60,21 +67,31 @@ public class GatewayControllerIntTest {
     @Value("${course-service.url}")
     private String courseServiceUrl;
 
+    private StudentDTO studentDTO;
+    private CourseDTO courseDTO;
+
     @Before
     public void init() {
         mockServer = MockRestServiceServer.createServer(restTemplate);
+
+        courseDTO = new CourseDTO();
+        courseDTO.setName("Chemistry");
+        courseDTO.setId(UUID.randomUUID().toString());
+
+        studentDTO = new StudentDTO();
+        studentDTO.setId(UUID.randomUUID().toString());
+        studentDTO.setFirstName("Jennifer");
+        studentDTO.setLastName("Lawrence");
     }
 
     @Test
     public void getAllCourses() throws Exception {
-        CourseDTO courseDTO = new CourseDTO();
-        courseDTO.setId(UUID.randomUUID().toString());
-        courseDTO.setName("Bio");
+
         ArrayList<CourseDTO> courseDTOS = new ArrayList<>();
         courseDTOS.add(courseDTO);
 
         mockServer.expect(ExpectedCount.once(),
-                        requestTo(new URI("http://localhost:9002/courses")))
+                        requestTo(new URI(courseServiceUrl)))
                 .andExpect(method(HttpMethod.GET))
                 .andRespond(withStatus(HttpStatus.OK)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -83,42 +100,24 @@ public class GatewayControllerIntTest {
 
         mvc.perform(get("/courses"))
                 .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void saveCourse() throws Exception{
-    }
-
-    @Test
-    public void findCourseById() throws Exception{
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(1)));
     }
 
     @Test
     public void findCoursesByStudentId() throws Exception{
-        final String studentId = UUID.randomUUID().toString();
-        StudentDTO studentDTO = new StudentDTO();
-        studentDTO.setId(studentId);
-        studentDTO.setFirstName("Jennifer");
-        studentDTO.setLastName("Lawrence");
-
         mockServer.expect(ExpectedCount.once(),
-                requestTo(new URI(String.format("%s/%s", studentServiceUrl, studentId))))
+                requestTo(new URI(String.format("%s/%s", studentServiceUrl, studentDTO.getId()))))
                 .andExpect(method(HttpMethod.GET))
                 .andRespond(withStatus(HttpStatus.OK)
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(mapper.writeValueAsString(studentDTO)));
 
         CourseSearchRequestDTO searchRequestDTO = new CourseSearchRequestDTO();
-        searchRequestDTO.setStudentId(studentId);
+        searchRequestDTO.setStudentId(studentDTO.getId());
 
-        CourseDTO courseDTO = new CourseDTO();
-        courseDTO.setName("Chemistry");
-        courseDTO.setId(UUID.randomUUID().toString());
         List<CourseDTO> courseDTOS = new ArrayList<>();
         courseDTOS.add(courseDTO);
-
-
 
         mockServer.expect(ExpectedCount.once(),
                 requestTo(new URI(String.format("%s/students",courseServiceUrl))))
@@ -128,7 +127,7 @@ public class GatewayControllerIntTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(mapper.writeValueAsString(courseDTOS)));
 
-        mvc.perform(get(String.format("/courses/students/%s",studentId)))
+        mvc.perform(get(String.format("/courses/students/%s",studentDTO.getId())))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].id",
@@ -139,21 +138,62 @@ public class GatewayControllerIntTest {
 
     @Test
     public void addStudentToCourse() throws Exception{
-    }
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI(String.format("%s/%s",studentServiceUrl,studentDTO.getId()))))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(studentDTO)));
 
-    @Test
-    public void findAllStudents() throws Exception{
-    }
+        CourseWithStudentDTO courseWithStudentDTO = new CourseWithStudentDTO();
+        courseWithStudentDTO.setCourse(courseDTO);
+        courseWithStudentDTO.setStudent(studentDTO);
 
-    @Test
-    public void saveStudent() throws Exception{
-    }
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI(String.format("%s/%s/%s",courseServiceUrl,courseDTO.getId(),studentDTO.getId()))))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(courseDTO)));
 
-    @Test
-    public void findStudentById() throws Exception{
+        mvc.perform(post(String.format("/courses/%s/%s",courseDTO.getId(),studentDTO.getId())))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.course.id",
+                        is(courseDTO.getId())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.student.id",
+                        is(studentDTO.getId())));
     }
 
     @Test
     public void findStudentsByCourseId() throws Exception{
+        ItemIdsDTO itemIdsDTO = new ItemIdsDTO(List.of(studentDTO.getId()));
+
+        mockServer.expect(ExpectedCount.once(),
+                        requestTo(new URI(String.format("%s/%s", courseServiceUrl, courseDTO.getId()))))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(courseDTO)));
+
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI(String.format("%s/students/%s", courseServiceUrl, courseDTO.getId()))))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(List.of(studentDTO.getId()))));
+
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI(String.format("%s/ids", studentServiceUrl))))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(MockRestRequestMatchers.content().string(asJsonString(itemIdsDTO)))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(List.of(studentDTO))));
+
+        mvc.perform(get(String.format("/students/courses/%s", courseDTO.getId())))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(studentDTO.getId())));
     }
 }
